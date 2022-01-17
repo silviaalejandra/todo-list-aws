06592aeb0777b2bcb28e35c4c0d682785d43b612
silviaalejandra <97700312+silviaalejandra@users.noreply.github.com>
2022-01-17 15:45:35 -0300

    Se agrega comentario de eliminación del return

--- src/update.py
+++ src/update.py
@@ -9,17 +9,18 @@ def update(event, context):
     if 'text' not in data or 'checked' not in data:
         logging.error("Validation Failed")
         raise Exception("Couldn't update the todo item.")
+        #SG20220117 Se elimina debido a la salida por el rise exception
         #return
-    else:
-        # update the todo in the database
-        result = todoList.update_item(
-            event['pathParameters']['id'],
-            data['text'], data['checked'])
-        # create a response
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(result,
-                               cls=decimalencoder.DecimalEncoder)
-        }
 
-        return response
+    # update the todo in the database
+    result = todoList.update_item(
+        event['pathParameters']['id'],
+        data['text'], data['checked'])
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result,
+                           cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
