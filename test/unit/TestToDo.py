4d9ef4972e16056159a316862b329a084c99ba3e
Gonzalez, Silvia Alejandra <silviaalejandra@gmail.com>
2022-01-18 16:11:15 +0000

    cambio caso cobertura update_item6

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -79,7 +79,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         # Testing file functions
         from src.todoList import put_item
         # Table mock
-        self.assertRaises(Exception, put_item(1 + '1', self.dynamodb))
+        self.assertRaises(TypeError, put_item(1 + '1', self.dynamodb))
         # self.assertRaises(Exception, put_item("", self.dynamodb))
         print ('End: test_put_todo_error')
 
