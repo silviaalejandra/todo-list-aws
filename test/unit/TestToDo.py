95c85f02047f1a17cbd39cfa9a5963076225ff19
Gonzalez, Silvia Alejandra <silviaalejandra@gmail.com>
2022-01-18 18:33:13 +0000

    Se cambia los string vacíos por nulos para forzar la falla6

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -79,8 +79,8 @@ class TestDatabaseFunctions(unittest.TestCase):
         # Testing file functions
         from src.todoList import put_item
         # Table mock
-        self.assertRaises(Exception, put_item(None, self.dynamodb))
-        self.assertRaises(Exception, put_item(None, self.dynamodb))
+        self.assertRaises(TypeError, put_item(1+'1', self.dynamodb))
+        #self.assertRaises(Exception, put_item(None, self.dynamodb))
         print ('End: test_put_todo_error')
 
     def test_get_todo(self):
