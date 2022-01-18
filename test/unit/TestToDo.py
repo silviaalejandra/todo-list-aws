34c2290c2ac7ed11ec73913caf0640da0b234740
Gonzalez, Silvia Alejandra <silviaalejandra@gmail.com>
2022-01-18 17:25:54 +0000

    Se cambia los string vacíos por nulos para forzar la falla1

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -197,7 +197,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         print ('Start: test_delete_todo_error')
         from src.todoList import delete_item
         # Testing file functions
-        self.assertRaises(TypeError, delete_item("", self.dynamodb))
+        self.assertRaises(TypeError, delete_item(None, self.dynamodb))
         print ('End: test_delete_todo_error')
 
 
