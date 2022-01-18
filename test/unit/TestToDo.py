afa74793bb566020f9e0a1b232fe4a18b5e61371
Gonzalez, Silvia Alejandra <silviaalejandra@gmail.com>
2022-01-18 17:52:56 +0000

    Se cambia los string vacíos por nulos para forzar la falla4

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -80,7 +80,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         from src.todoList import put_item
         # Table mock
         self.assertRaises(Exception, put_item(None, self.dynamodb))
-        #self.assertRaises(Exception, put_item("", self.dynamodb))
+        self.assertRaises(Exception, put_item(None, self.dynamodb))
         print ('End: test_put_todo_error')
 
     def test_get_todo(self):
@@ -104,6 +104,23 @@ class TestDatabaseFunctions(unittest.TestCase):
             self.text,
             responseGet['text'])
         print ('End: test_get_todo')
+    
+    def test_get_todo_error(self):
+        print ('---------------------')
+        print ('Start: test_get_todo_error')
+        # Testing file functions
+        from src.todoList import get_item
+        from src.todoList import put_item
+        
+        responsePut = put_item(self.text, self.dynamodb)
+        print ('Response put_item:' + str(responsePut))
+        idItem = json.loads(responsePut['body'])['id']
+        print ('Id item:' + idItem)
+        
+        # Table mock
+        self.assertRaises(Exception, get_item(None, self.dynamodb))
+        self.assertRaises(TypeError, get_item(None, self.dynamodb))
+        print ('End: test_get_todo_error')
     
     def test_list_todo(self):
         print ('---------------------')
