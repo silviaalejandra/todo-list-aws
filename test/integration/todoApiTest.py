9fe8e5d008520a754abb1ffa808fbb9b4b0ed4c7
Gonzalez, Silvia Alejandra <silviaalejandra@gmail.com>
2022-01-20 23:46:44 -0300

    test de integracion Translate

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -112,6 +112,46 @@ class TestApi(unittest.TestCase):
         )
         print('End - integration test Get TODO')
     
+    #Test de Translate-----------------------------------------------
+    #----------------------------------------------------------------
+    def test_api_translatetodo(self):
+        print('---------------------------------------')
+        print('Starting - integration test Translate TODO')
+        #Add TODO
+        url = BASE_URL+"/todos"
+        data = {
+         "text": "Integration text example - TRADUCIR"
+        }
+        response = requests.post(url, data=json.dumps(data))
+        json_response = response.json()
+        print('Response Add Todo: '+ str(json_response))
+        jsonbody= json.loads(json_response['body'])
+        ID_TODO = jsonbody['id']
+        print ('ID todo:'+ID_TODO)
+        self.assertEqual(
+            response.status_code, 200, "Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            jsonbody['text'], "Integration text example - TRADUCIR", "Error en la petición API a {url}"
+        )
+        #Test Translate TODO
+        url = BASE_URL+"/todos/"+ID_TODO+/"it"
+        response = requests.get(url)
+        json_response = response.json()
+        print('Response Translate Todo: '+ str(json_response))
+        self.assertEqual(
+            response.status_code, 200, "Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            json_response['text'], "Esempio di testo di integrazione - TRANSL", "Error en la petición API a {url}"
+        )
+        #Delete TODO to restore state
+        response = requests.delete(url)
+        self.assertEqual(
+            response.status_code, 200, "Error en la petición API a {url}"
+        )
+        print('End - integration test Translate TODO')
+    #----------------------------------------------------------------
     def test_api_updatetodo(self):
         print('---------------------------------------')
         print('Starting - integration test Update TODO')
