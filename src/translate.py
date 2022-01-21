4f43781da3ea679c6cafa27a1e8560b36b582c34
Gonzalez, Silvia Alejandra <silviaalejandra@gmail.com>
2022-01-21 00:49:52 -0300

    Se separan funciones de translate3

--- src/translate.py
+++ src/translate.py
@@ -31,7 +31,7 @@ def lambda_handler(event, context):
         logger.info(source_language)
 
         # Ordeno la lista de lenguajes por el mejor score
-        # order_languaje = sorted(source_language["Languages"], key=lambda k: k['Score'], reverse=True) 
+        # order_languaje = sorted(source_language["Languages"], key=lambda k: k['Score'], reverse=True)
         # logger.info(order_languaje)
 
         # Obtengo el primero de la lista ordenada
