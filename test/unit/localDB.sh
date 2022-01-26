e5f2191d0b8aa9b7295c0ee6b7c254e72f0f5de9
Gonzalez, Silvia Alejandra <silviaalejandra@gmail.com>
2022-01-26 02:41:01 +0000

    correcciones en el test para el puerto de la base local y archivo de docker3

--- test/unit/localDB.sh
+++ test/unit/localDB.sh
@@ -3,10 +3,10 @@
 #  de regresion que requieren una base local
 
 ## Crear red de docker
-docker network create samtest
+sudo docker network create samtest
 
 ## Levantar el contenedor de dynamodb en la red de sam con el nombre de dynamodb
-docker run -p 8001:8000 --network samtest --name dynamodb2 -d amazon/dynamodb-local
+sudo docker run -p 8001:8000 --network samtest --name dynamodb2 -d amazon/dynamodb-local
 
 ## Crear la tabla en local, para poder trabajar localmemte
-aws dynamodb create-table --table-name localtest-DynamoDbTable --region us-east-1 --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --endpoint-url http://localhost:8001
+sudo aws dynamodb create-table --table-name localtest-DynamoDbTable --region us-east-1 --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --endpoint-url http://localhost:8001
